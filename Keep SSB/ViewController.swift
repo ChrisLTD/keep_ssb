1b8e8e6da28b7cc2115aca28e796adcd9dfeb7a0
Chris Johnson <chrisltd@gmail.com>
2019-07-15 11:41:10 -0400

    opening external links in Safari

--- Keep SSB/ViewController.swift
+++ Keep SSB/ViewController.swift
@@ -9,11 +9,12 @@
 import Cocoa
 import WebKit
 
-class ViewController: NSViewController, WKUIDelegate {
+class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate {
 
     var webView: WKWebView!
     
     let webUrl = "https://keep.google.com"
+    let baseDomain = "keep.google.com"
     let webName = "Google Keep"
     
     override func loadView() {
@@ -22,6 +23,7 @@ class ViewController: NSViewController, WKUIDelegate {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
+        webView.navigationDelegate = self
         view = webView
     }
     
@@ -38,5 +40,27 @@ class ViewController: NSViewController, WKUIDelegate {
     
         self.view.window?.title = webName
     }
+    
+    // Open external links in Safari
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        if navigationAction.navigationType != .linkActivated {
+            // not a click
+            decisionHandler(.allow)
+            return
+        }
+        
+        let url = navigationAction.request.url
+        
+        if url?.description.lowercased().range(of: baseDomain) != nil {
+            // open in app
+            decisionHandler(.allow)
+        } else {
+            // open in Safari
+            decisionHandler(.cancel)
+            NSWorkspace.shared.open(url!)
+        }
+        
+    }
+    
 }
-
